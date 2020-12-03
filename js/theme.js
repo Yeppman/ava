63ad0eae1ae54dc10c1ee36503e0bc62bddea2dc
Yeppman <yeppman13@gmail.com>
2020-12-03 15:06:52 +0100

    new design

--- js/theme.js
+++ js/theme.js
@@ -15,7 +15,7 @@
                 if (scroll >= nav_offset_top ) {
                     $(".header_area").addClass("navbar_fixed");
                 } else {
-                    $(".header_area").removeClass("navbar_fixed");
+                    $(".header_area").addClass("navbar_fixed");
                 }
             });
         };
